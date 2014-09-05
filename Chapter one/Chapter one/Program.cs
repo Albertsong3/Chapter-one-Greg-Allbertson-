1d01800b0bd31581eb49142ba2b59783591079c8
Albertsong3 <gregalbertson@rocketmail.com>
2014-09-05 08:14:35 -0400

    First version

---
+++ Chapter one/Chapter one/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chapter_one
+{
+    class Program
+    {
+        public static void Main ()
+        {
+            Console.WriteLine("first one");
+        }
+    }
+}
